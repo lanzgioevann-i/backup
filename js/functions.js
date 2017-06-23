e4c1bf95814213cb1380a2144b1a7f96d51b6dbc
lazygeniuslanz <lanz.irlanda@gmail.com>
2017-06-23 22:16:44 +0800

    init

---
+++ js/functions.js
@@ -0,0 +1,42 @@
+function navshow(){
+	//nav show
+	var window_top = $(window).scrollTop();
+	var div_top = $('#line').offset().top;
+	/*
+	*/
+	if(window_top > div_top){ 
+		$('#navbar').addClass('shownav');
+		$('#navbar').removeClass('hidenav');
+	}
+	else{
+		$('#navbar').removeClass('shownav');
+		$('#navbar').addClass('hidenav');
+	}
+}
+
+$(function() { //nav show
+  $(window).scroll(navshow);
+});
+
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+jQuery(document).ready(function($){
+ // Defining a function to set size for #hero 
+	function fullscreen(){
+		jQuery('#hero').css({
+            width: jQuery(window).width(),
+            height: jQuery(window).height()
+        });
+	}
+  
+	fullscreen();
+
+  // Run the function in case of window resize
+  jQuery(window).resize(function() {
+       fullscreen();         
+    });
+
+});
